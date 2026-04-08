ef8c7c247b532538d5fecf6ea65080cdf76c0db8
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-08 13:39:28 +0000

    Apr 8, 2026, 3:39 PM

--- TestProject1/obj/Debug/net481/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net481/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7899028af1067077d9824f88c15fc00b8ee06d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c4c0bd9a2eee7eef2d8cadf14c2711ec11915e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
