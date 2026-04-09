30ed40e3a866cfee918dcedff727d3ecc5ae3a22
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-09 19:15:36 +0000

    Apr 9, 2026, 9:15 PM

--- FlaUITests/Program.cs
+++ FlaUITests/Program.cs
@@ -1,12 +1,9 @@
 using System;
 
-namespace FlaUITests
-{
-    class Program
-    {
+namespace FlaUITests {
+    class Program {
         static AutomationStudio6 as6;
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             as6 = new AutomationStudio6();
             as6.OpenApplication();
             while (!as6.IsProjectLoaded()) {
@@ -15,9 +12,8 @@ namespace FlaUITests
                 System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
             }
             as6.CloseProject();
-            as6.ReadProject();
             as6.OpenProject("C:\\Users\\ATDIBRU\\OneDrive - ABB\\projects\\test_Mot");
-            as6.FileMenu.Click();
+            as6.ReadProject();
             as6.CloseApplication();
         }
     } 
