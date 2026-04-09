7c83fdb6a1f79d3efbb5442cd9035a933900bc11
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-09 20:18:46 +0000

    Apr 9, 2026, 10:18 PM

--- FlaUITests/AutomationStudio6.cs
+++ FlaUITests/AutomationStudio6.cs
@@ -90,7 +90,8 @@ namespace FlaUITests {
                 AutomationElement pane3 = openProjectDialog.FindFirstChild(cf => cf.ByControlType(ControlType.Pane).And(cf.ByAutomationId("40965")));
                 AutomationElement comboBox = pane3.FindFirstDescendant(cf => cf.ByControlType(ControlType.Pane).And(cf.ByAutomationId("41477")));
                 ProgressBar progressBar = comboBox.FindFirstChild(cf => cf.ByControlType(ControlType.ProgressBar)).AsProgressBar();
-                AutomationElement [] children = progressBar.FindAllChildren();
+                AutomationElement toolBar = comboBox.FindFirstChild(cf => cf.ByControlType(ControlType.ToolBar));
+                AutomationElement [] children = toolBar.FindAllChildren();
                 TextBox folderTextBox = comboBox.FindFirstChild(cf => cf.ByControlType(ControlType.Edit).And(cf.ByName("Address"))).AsTextBox();
                 folderTextBox.Text = projectPath + "\n"; // Enter the project path
                 Button openButton = openProjectDialog.FindFirstDescendant(cf => cf.ByControlType(ControlType.Button).And(cf.ByAutomationId("1"))).AsButton();
