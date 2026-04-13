64d9db5eb43f040c4bdcf0fa0605fcc9f7dd07fe
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-13 15:16:31 +0000

    Apr 13, 2026, 5:16 PM

--- FlaUITests/Util/MappViewProject.cs
+++ FlaUITests/Util/MappViewProject.cs
@@ -1,10 +1,12 @@
 using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Definitions;
 using System;
+using System.Linq;
 
 namespace FlaUITests.Util {
     public class MappViewProject : AppProject {
         AutomationElement toolbox;
+        AutomationElement outputWindow;
         AutomationElement toolBoxCategories;
         AutomationElement toolBoxContextContent;
         public MappViewProject(IDE_Main ideMain) : base(ideMain) {
@@ -30,7 +32,39 @@ namespace FlaUITests.Util {
                 System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
             }
             AutomationElement mappViewElementItem = toolBoxContextContent.FindFirstDescendant(cf => cf.ByControlType(ControlType.DataItem).And(cf.ByName("mapp View"))) ?? throw new Exception("mapp View element not found");
-            mappViewElementItem.DoubleClick();
+            mappViewElementItem.DoubleClick();  //mapp View wizard opens
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            Window newMappViewDialog = _ideMain.GetModalWindow("Insert mapp View solution");
+            AutomationElement defaultTemplate = null;
+            AutomationElement [] allElements = newMappViewDialog.FindAllDescendants();
+            foreach (var element in allElements) {
+                string childName = element.Name;
+                if (childName.IndexOf("Default", StringComparison.OrdinalIgnoreCase) >= 0)
+                    defaultTemplate = element;
+            }
+            if (defaultTemplate == null) {
+                Console.WriteLine("Default template not found in mapp View wizard");
+                return;
+            }
+            AutomationElement [] allTemplates = defaultTemplate.Parent.FindAllChildren();
+            Random rand = new Random();
+            int index = rand.Next(allTemplates.Length);
+            allTemplates[index].DoubleClick(); //Select a random template to create some variation in the created projects
+            outputWindow = _ideMain.OutputWindow;
+            if (outputWindow == null) {
+                _ideMain.InvokeMenuItem(_ideMain.GetMenu("View"), "Output", "Output Results");
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+                outputWindow = _ideMain.MainWindow.FindFirstDescendant(cf => cf.ByControlType(ControlType.Pane).And(cf.ByName("Output")));
+            }
+            bool done = false;
+            while (!done) {
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+                AutomationElement outputListView = outputWindow.FindFirstDescendant(cf => cf.ByControlType(ControlType.DataGrid).And(cf.ByAutomationId("outputListView")));
+                AutomationElement [] allMessages = outputListView.FindAllDescendants(cf => cf.ByControlType(ControlType.DataItem));
+                AutomationElement [] allMessagesTexts = allMessages[allMessages.Length - 1].FindAllDescendants(cf => cf.ByControlType(ControlType.Text));
+                if (allMessagesTexts.Any(m => m.Name.IndexOf("Parsing finished", StringComparison.OrdinalIgnoreCase) >= 0))
+                    done = true;
+            }
         }
     }
 }
