078a16871f72f78eda48e2d7fd427c20e61f74e1
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-13 06:30:32 +0000

    Apr 13, 2026, 8:30 AM

--- FlaUITests/AutomationStudio6.cs
+++ FlaUITests/AutomationStudio6.cs
@@ -1,37 +1,23 @@
 using System;
-using System.Drawing;
-using System.Linq;
 using FlaUI.Core;
 using FlaUI.Core.AutomationElements;
-using FlaUI.Core.Conditions;
-using FlaUI.Core.Definitions;
-using FlaUI.Core.Input;
 using FlaUI.UIA2;
 using FlaUITests.Util;
-using Menu = FlaUI.Core.AutomationElements.Menu;
 
 namespace FlaUITests {
     public class AutomationStudio6 {
         private Application _app;
-        private UIA2Automation _automation;
-        private Window _mainWindow;
-        private ConditionFactory _cf;
         private IDE_Main _ideMain;
-
+        private Project _project;
 
         /// <summary>
-        /// Opens the Automation Studio 6 application and initializes all IDE items
+        /// Opens the Automation Studio 6 application and initializes main items
         /// </summary>
         public void OpenApplication() {
             _app = Application.Launch(@"C:\Program Files (x86)\BRAutomation\AS6\bin-en\pg.exe");
             _app.WaitWhileMainHandleIsMissing(TimeSpan.FromSeconds(20));
             _app.WaitWhileBusy(TimeSpan.FromSeconds(20));
-
-            _automation = new UIA2Automation();
-            _mainWindow = _app.GetMainWindow(_automation);
-            _cf = new ConditionFactory(new UIA2PropertyLibrary());
-            _ideMain = new IDE_Main(_app, _mainWindow, _automation, _cf);
-
+            _ideMain = new IDE_Main(_app);
         }
         /// <summary>
         /// Closes the Automation Studio 6 application
