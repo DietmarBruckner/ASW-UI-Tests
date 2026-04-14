03cabfc051f32e048bba413f34e1574d2e06abdf
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-14 20:46:27 +0000

    Apr 14, 2026, 10:46 PM

--- FlaUITests/Util/MappViewProject.cs
+++ FlaUITests/Util/MappViewProject.cs
@@ -78,6 +78,7 @@ namespace FlaUITests.Util {
             ClickComboBoxTreeItem(1); //Select "Enabled"
             System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1)); */
             AutomationElement [] buttons = _ideMain.ToolBarStandard.FindAllDescendants(cf => cf.ByControlType(ControlType.Button));
+            AutomationElement saveButton = _ideMain.ToolBarStandard.FindFirstDescendant(cf => cf.ByControlType(ControlType.Button).And(cf.ByName("BR_\nSave"))).AsButton();
             _ideMain.ToolBarStandard.FindFirstDescendant(cf => cf.ByControlType(ControlType.Button).And(cf.ByName("BR_\nSave"))).AsButton().Invoke();
             while (_ideMain.StatusBar.Name.IndexOf("Saving", StringComparison.OrdinalIgnoreCase) >= 0);
         }
--- FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
+++ FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc60d09915f9b23b750fef19143671fcb70cd90f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d8293b592230051b15da9d14349b59afac0603")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
