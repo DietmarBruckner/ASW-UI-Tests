98eab6de3a69671911ecc0ea059beb2cd819d6eb
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-15 09:29:27 +0000

    Apr 15, 2026, 11:29 AM

--- FlaUITests/Util/TreeConfig.cs
+++ FlaUITests/Util/TreeConfig.cs
@@ -39,11 +39,11 @@ namespace FlaUITests.Util {
             foreach (var sub in leaves) {
                 AutomationElement oldAe = ae;
                 ae = oldAe.FindFirstChild(cf => cf.ByControlType(ControlType.TreeItem).And(cf.ByName(sub)));
-                AutomationElement [] allChildren = ae.FindAllChildren(cf => cf.ByControlType(ControlType.TreeItem));
+                //AutomationElement [] allChildren = ae.FindAllChildren(cf => cf.ByControlType(ControlType.TreeItem));
                 ClickConfigTreeItem(ae, toClickSubstrings[Array.IndexOf(leaves, sub)], true);
                 System.Threading.Thread.Sleep(TimeSpan.FromMilliseconds(500));
                 ae = oldAe.FindFirstChild(cf => cf.ByControlType(ControlType.TreeItem).And(cf.ByName(sub)));
-                allChildren = ae.FindAllChildren(cf => cf.ByControlType(ControlType.TreeItem));
+                //allChildren = ae.FindAllChildren(cf => cf.ByControlType(ControlType.TreeItem));
             }
 /*             ae = ae.FindFirstChild(cf => cf.ByControlType(ControlType.TreeItem).And(cf.ByName("BR_" + CPU)));
             ClickConfigTreeItem(ae, "_Configuration", true);
--- FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
+++ FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c113a13c1e509bf91316ed1a60bd3b43b966556d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+139fb77a3ea0c94d038580b39a37f9de6ba9c154")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
