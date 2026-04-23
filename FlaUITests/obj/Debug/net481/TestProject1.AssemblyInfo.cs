502e050465a24f738fc8b5885f395727d724748e
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-23 06:23:06 +0000

    Apr 23, 2026, 8:23 AM

--- FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
+++ FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc528cb9ed75a24649f150edbc29b90ebdc2c9ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea35e67a9818267c05f32cbb8b92dd51ced3ae39")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
