47da383e70d92016eaa19b9225d775cd46b03123
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-23 06:14:48 +0000

    Apr 23, 2026, 8:14 AM

--- FlaUITests/Util/IDE_Main.cs
+++ FlaUITests/Util/IDE_Main.cs
@@ -397,7 +397,7 @@ namespace FlaUITests.Util {
  */             InvokeMenuItem(GetMenu("Online"), "Activate Simulation");
         }
         bool IsButtonActive(Button button, string image) {
-            Capture.Element(button).ToFile( @"$(ProjectDir)\\Util\\screenshots\\" + image + ".png");
+            Capture.Element(button).ToFile( @"$(ProjectDir)\Util\screenshots\" + image + ".png");
             Image actual = Image.FromFile(  @"$(ProjectDir)\\Util\\screenshots\\" + image + ".png");
             Image active = Image.FromFile(  @"$(ProjectDir)\\Util\\Buttons\\" + image + "_active.png");
             Image inactive = Image.FromFile(@"$(ProjectDir)\\Util\\Buttons\\" + image + "_inactive.png");
--- FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
+++ FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dad1d425eb141e198d1df34b0922a77bc2d6eb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1dacb9bb76f0a12774c84592cf09feed4fa365")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
