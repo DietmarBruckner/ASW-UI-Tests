656baeb76699534b1b65cb7040bda91d2fa0cf3d
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-04-27 11:36:02 +0000

    Apr 27, 2026, 1:36 PM

--- FlaUITests/Util/Environment.cs
+++ FlaUITests/Util/Environment.cs
@@ -2,6 +2,8 @@ using System.Security.Policy;
 
 namespace FlaUITests.Util {
     public static class Environment {
+        public enum Verbose {NONE, LIGHT, STEPS, FULL}
         public static string InstallationPath;
+        public static Verbose verbose;
     }
 }
