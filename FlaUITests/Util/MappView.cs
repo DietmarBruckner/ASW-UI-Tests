63e3ec03322e33d3d212749033aeffc7e14b50cb
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-05-04 12:35:39 +0000

    May 4, 2026, 2:35 PM

--- FlaUITests/Util/MappView.cs
+++ FlaUITests/Util/MappView.cs
@@ -63,6 +63,8 @@ namespace FlaUITests.Util {
         void TM611_3_2_ConfigureMappViewServer() {
             string mvconfig = "Config.mappviewcfg";
             TreeConfig.ActivateTreeLeaf(TreeConfig.ViewType.ConfigurationView, new List<string> { "BR_" + Project.CPU, "BR_mappView"}, new List<string> { "_Configuration", "_Configuration" });
+            AutomationElement asd = TreeConfig.IdeMain.GetActiveConfigurtion().FindAllDescendants(cf => cf.ByControlType(ControlType.TreeItem)).First(cf => cf.Name.IndexOf("mappView") >= 0);
+            AutomationElement [] asdsdf = asd.FindAllChildren();
             if (TreeConfig.IdeMain.GetActiveConfigurtion().FindAllDescendants(cf => cf.ByControlType(ControlType.TreeItem)).First(cf => cf.Name.IndexOf("mappView") >= 0).FindAllChildren(cf => cf.ByName(mvconfig)) == null) {
                 if (Verbose >= Util.Environment.Verbose.STEPS) {
                     Console.WriteLine("==========================================");
--- FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
+++ FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e1a60c476a639666f63ff21cc073a8eb26f2d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c66f940cfe9814ea74ab7d1ea9cf4c541764059e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
