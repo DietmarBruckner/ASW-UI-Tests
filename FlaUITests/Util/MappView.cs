0a38d84e7915b85fe1ab9dc072f7701091a06bf7
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-05-06 15:17:08 +0000

    May 6, 2026, 5:17 PM

--- FlaUITests/Util/MappView.cs
+++ FlaUITests/Util/MappView.cs
@@ -337,6 +337,7 @@ namespace FlaUITests.Util {
             EditSize(width:800, height:100, area:true);
             EditPosition(left:0, top:0, area:true);
  */            TreeConfig.ActivateTreeLeaf(TreeConfig.ViewType.LogicalView, new List<string> { "BR_mappView", "BR_Visualization", "BR_Pages", "BR_page_0", "BR_page_0.page"}, new List<string> { "_Object Name", "_Object Name", "_Object Name", "_Object Name", "_Object Name" });
+            System.Threading.Thread.Sleep(TimeSpan.FromMilliseconds(500));
             AutomationElement page_0ConfigWorkspaceWindow = TreeConfig.IdeMain.Workspace.FindAllChildren(cf => cf.ByControlType(ControlType.Window)).First(cf => cf.Name.IndexOf("page_0.page") >= 0);
             Mouse.Click(editorCenter);
             AutomationElement editor = page_0ConfigWorkspaceWindow.FindFirstDescendant(cf => cf.ByControlType(ControlType.Document).And(cf.ByName("Page-Editor")));
--- FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
+++ FlaUITests/obj/Debug/net481/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dcd20ba2b821b30fff3628f4968d30eeea2999e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc86d67def43ef9f24c779c4ff7fb5064a9a64e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
