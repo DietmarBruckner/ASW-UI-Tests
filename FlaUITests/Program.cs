d62c9c2fb7e2f8a857f4c2f7baa03848a41a4d72
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-05-06 13:26:56 +0000

    May 6, 2026, 3:26 PM

--- FlaUITests/Program.cs
+++ FlaUITests/Program.cs
@@ -9,13 +9,13 @@ namespace FlaUITests {
         static void Main(string[] args) {
             Util.Util.Environment.InstallationPath = "C:\\Program Files (x86)\\BRAutomation\\AS6";
             as6 = new AutomationStudio6();
-            //as6.Project.CloseProject();
-            //as6.Project.OpenProject("C:\\Users\\ATDIBRU\\OneDrive - ABB\\projects\\test_Mot");
             Dictionary<Components, string> dictcom = new Dictionary<Components, string> () {
-            //    {Components.OPCUACS, "6.6.1"},
+                {Components.OPCUACS, "6.6.1"},
                 {Components.mappView, "6.6.0"},
                 {Components.AutomationRuntime, "6.5.1"}
             };
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Creating new project with OPC UA, mapp View, Automation Runtime; CPU: X20CP1684, AS version: 6.3");
             as6.Project = new FlaUITests.Util.AppProject(as6.Ide_Main, "FlaUI_Test1", "C:\\Users\\ATDIBRU\\OneDrive - ABB\\projects\\", "Config1", "X20CP1684", dictcom, "6.3", Util.Util.Environment.Verbose.FULL);
             as6.Project.ReadProject();
             //as6.CloseApplication();
