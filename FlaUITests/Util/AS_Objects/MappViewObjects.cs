f2d3df2feb80365f8778506274a723106caf85cc
Dietmar Bruckner <dietmar.bruckner@at.abb.com>
2026-05-11 06:20:43 +0000

    May 11, 2026, 8:20 AM

--- FlaUITests/Util/AS_Objects/MappViewObjects.cs
+++ FlaUITests/Util/AS_Objects/MappViewObjects.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+
 namespace FlaUITests.Util.AS_Objects {
     public class MappViewObjects {
+        public List<MappViewPage> Pages;
         public bool [] ButtonValues = new bool [9];
         public string [][] ButtonValuesStrings;
+    }
+    public class MappViewPage {
+        public string Name;
+        public List<string> Widgets;
+        public MappViewPage(string name, List<string> widgets) {
+            Name = name;
+            Widgets = widgets;
+        }
     }
 }
