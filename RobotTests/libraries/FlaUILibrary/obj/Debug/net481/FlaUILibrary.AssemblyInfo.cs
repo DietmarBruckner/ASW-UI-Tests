c4698619ca70e3853cc63c5b39726d95e4e2ded1
Alexander Wimmer <alexander.wimmer2@at.abb.com>
2026-09-03 13:12:03 +0200

    cleanup old FlaUITests

--- RobotTests/libraries/FlaUILibrary/obj/Debug/net481/FlaUILibrary.AssemblyInfo.cs
+++ RobotTests/libraries/FlaUILibrary/obj/Debug/net481/FlaUILibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlaUILibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10cd5ad6391ef42afc5d42cfb10bcfed4bf0a8b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deca79422e9c143bd81e259196b1586980a12bc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlaUILibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlaUILibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
